71515777c7a185c1f0f607507360549c5941fd0d
Joshua Stone <joshua.gage.stone@gmail.com>
2014-10-26 17:54:57 -0400

    Add page property and setter for Lists

--- derpibooru/Lists.py
+++ derpibooru/Lists.py
@@ -12,3 +12,17 @@ class Lists(object)
   def parameters(self):
     return(self.__parameters)
 
+  @property
+  def page(self):
+    return(self.parameters["page"])
+
+  @page.setter
+  def page(self, page=1):
+    if not isinstance(page, int):
+      raise TypeError("page number must be an int")
+
+    if page < 1:
+      raise ValueError("page number must be greater than 1")
+
+    self.__parameters["page"] = page
+
