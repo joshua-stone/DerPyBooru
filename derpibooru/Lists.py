a1a700552bfd980ebb0891b61d0d211699e1babb
Joshua Stone <joshua.gage.stone@gmail.com>
2014-10-26 17:53:07 -0400

    Create initial Lists class

---
+++ derpibooru/Lists.py
@@ -0,0 +1,14 @@
+
+class Lists(object)
+
+  def __init__(self, page=1, last="", comments=False, fav=False, key=""):
+    self.__parameters = {}
+
+  @property
+  def hostname()
+    return("https://derpiboo.ru")
+
+  @property
+  def parameters(self):
+    return(self.__parameters)
+
