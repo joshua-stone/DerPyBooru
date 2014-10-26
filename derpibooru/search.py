c6e1c7bb0881aca0bd143fe065ed761c4bdeafa5
Joshua Stone <joshua.gage.stone@gmail.com>
2014-10-26 12:10:02 -0400

    add hostname and q properties to Search

--- derpibooru/search.py
+++ derpibooru/search.py
@@ -5,3 +5,12 @@ class Search(object):
     self.comments = comments
     self.fav = fav
     self.key = key
+
+  @property
+  def hostname(self):
+    return("https://derpiboo.ru")
+
+  @property
+  def q(self):
+    return(self.__q)
+
