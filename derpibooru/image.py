233090aac7d71a1a8e0caf348b3d785bca15f69c
Joshua Stone <joshua.gage.stone@gmail.com>
2014-10-29 01:48:33 -0400

    Create initial image.py file

---
+++ derpibooru/image.py
@@ -0,0 +1,111 @@
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+class Image(object):
+
+  def __init(self, data):
+    self.data = data
+
+  @property
+  def id(self):
+
+  @property
+  def tags(self):
+
+  @property
+  def is_optimized(self):
+
+  @property
+  def sha512_hash(self):
+
+  @property
+  def upvotes(self):
+
+  @property
+  def aspect_ratio(self):
+
+  @property
+  def original_format(self):
+
+  @property
+  def mime_type(self):
+
+  @property
+  def height(self):
+
+  @property
+  def updated_at(self):
+
+  @property
+  def width(self):
+
+  @property
+  def comment_count(self):
+
+  @property
+  def tag_ids(self):
+
+  @property
+  def created_at(self):
+
+  @property
+  def file_name(self):
+
+  @property
+  def uploader(self):
+
+  @property
+  def description(self):
+
+  @property
+  def orig_sha512_hash(self):
+
+  @property
+  def id_number(self):
+
+  @property
+  def license(self):
+
+  @property
+  def representations(self):
+
+  @property
+  def image(self):
+
+  @property
+  def score(self):
+
+  @property
+  def downvotes(self):
+
+  @property
+  def duplicate_reports(self):
+
+  @property
+  def faves(self):
+
+  @property
+  def source_url(self):
+
+  @property
+  def is_rendered(self):
+
+  @property
+  def data(self):
+    return(self.__data)
+
+  @data.setter
+  def data(self, data):
+    self.__data = data
+
