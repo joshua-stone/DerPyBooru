0b54797e79709cb2d93c9836d49a57bb1922c46e
Joshua Stone <joshua.gage.stone@gmail.com>
2014-11-03 22:42:23 -0500

    Remove summary, copyright, and email from setup.py to stop pip warnings

--- setup.py
+++ setup.py
@@ -7,19 +7,16 @@ import derpibooru
 
 setup(
   name = "PyDerpibooru",
-  summary = "Python wrapper for Derpibooru's API",
   url = "https://github.com/joshua-stone/pyderpibooru",
   version = "0.1",
   author = "Joshua Stone",
-  email = "joshua.gage.stone@gmail.com",
   license = "Simplified BSD License",
-  copyright = "Copyright 2014 Joshua Stone",
   platforms = ["any"],
   packages = find_packages(),
   include_package_data = True,
   classifiers = [
     "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
+    "License :: OSI Approved :: Simplified BSD License",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Internet"
   ]
