a5ae427004e98c21c623577099ce341bca86765f
Joshua Stone <joshua.gage.stone@gmail.com>
2014-11-08 22:33:53 -0500

    Create new Search() query-building class

--- derpibooru/query.py
+++ derpibooru/query.py
@@ -1,53 +1,33 @@
-def Derpibooru(key=(), q=[], comments=False, fav=False, perpage=50,
-               random=False, last=(0,"h")):
-  parameters = {
-    "key": key,
-    "q": q,
-    "comments": comments,
-    "fav": fav,
-    "perpage": perpage, 
-    "random": random,
-    "last": last
-  }
-  return Query(parameters)
-
-class Query(object):
-  def __init__(self, data={}):
-    self._parameters = data
+class Search(object):
+  def __init__(self, key={}, q=[], sf="created_at", sd="desc"):
+    self._parameters = {
+      "key": key,
+      "q": q,
+      "sf": sf,
+      "sd": sd
+    }
 
   @property
   def parameters(self):
     return self._parameters
 
   def api_key(self, key=""):
-    self.parameters["key"] = ("api_key", key)
-    return Query(self.parameters)
+    self._parameters["key"] = {"api_key": key}
+    return Search(**self._parameters)
 
   def user_id(self, user_id=""):
-    self.parameters["key"] = ("user_id", key)
-    return Query(self.parameters)
+    self._parameters["key"] = {"user_id": key}
+    return Search(**self._parameters)
 
-  def q(self, q=[]):
-    self.parameters["q"] = [str(tag).strip() for tag in q]
-    return Query(self.parameters)
+  def q(self, q):
+    self._parameters["q"] = [str(tag).strip() for tag in q]
+    return Search(**self._parameters)
 
-  def comments(self, comments=False):
-    self.parameters["comments"] = comments
-    return Query(self.parameters)
+  def descending(self):
+    self._parameters["sd"] = "desc" 
+    return Search(**self._parameters)
 
-  def fav(self, fav=False):
-    self.parameters["fav"] = fav
-    return Query(self.parameters)
-
-  def perpage(self, perpage=50):
-    self.parameters["perpage"] = perpage
-    return Query(self.parameters)
-
-  def random(self, random=True):
-    self.parameters["random"] = random
-    return Query(self.parameters)
-
-  def last(self, last=(0,"h")):
-    self.parameters["last"] = last
-    return Query(self.parameters)
+  def ascending(self):
+    self._parameters["sd"] = "asc"
+    return Search(**self._parameters)
 
