60defc25a5437e52f4616187f83b6a62e8d909d4
Joshua Stone <joshua.gage.stone@gmail.com>
2014-12-03 20:47:08 -0500

    Migrate unit tests from unittest to py.test for easier and more flexible testing

--- tests/test_search_live.py
+++ tests/test_search_live.py
@@ -1,63 +1,47 @@
-from unittest import TestCase, main
 from derpibooru import Search
 
-class Test_Search_Live(TestCase):
-  def test_query(self):
-    """
-    Test a query and make sure it returns correct image count and all images
-    are valid
-    """
-    limit, tag = 10, "sunset shimmer"
-    images = [image for image in Search().query(tag).limit(limit)]
+def test_query():
+  """
+  Tests whether the results in a query contain the tag that was being searched
+  for
+  """
+  limit, tag = 10, "sunset shimmer"
+  images = [image for image in Search().query(tag).limit(limit)]
 
-    self.assertEqual(len(images), limit)
-    
-    for image in images:
-      self.assertIsNotNone(image)
-      self.assertTrue(tag in image.tags)
+  assert len(images) == limit
 
-  def test_ascending(self):
-    """
-    Tests the functionality of ascending search
-    """
-    limit = 10
-    images = [image for image in Search().ascending().limit(limit)]
+  for image in images:
+    assert tag in image.tags
 
-    self.assertEqual(len(images), limit)
+def test_ascending():
+  """
+  Tests whether ascending search is in the correct order
+  """
+  limit = 10
+  images = [image for image in Search().ascending().limit(limit)]
 
-    for image in images:
-      self.assertIsNotNone(image)
-      """
-      Must return images with IDs from 0 to 10
-      """
-      self.assertLessEqual(image.id_number, 10)
+  assert len(images) == limit
 
-      """
-      Check if the images are in sequential order
-      by comparing the ID of the next image
-      """
-      if image is not images[-1]:
-        next_image = images[images.index(image) + 1]
-        self.assertLess(image.id_number, next_image.id_number)
+  for image in images:
+    # Check if the images are in ascending order
+    # by comparing the ID of the next image 
+    if image is not images[-1]:
+      next_image = images[images.index(image) + 1]
+      assert image.id_number < next_image.id_number
 
-  def test_descending(self):
-    """
-    Tests the functionality of descending search
-    """
-    limit = 10
-    images = [image for image in Search().descending().limit(limit)]
+def test_descending():
+  """
+  Tests whether descending search is in the correct order
+  """
+  limit = 10
+  images = [image for image in Search().descending().limit(limit)]
 
-    self.assertEqual(len(images), limit)
+  assert len(images) == limit
 
-    for image in images:
-      self.assertIsNotNone(image)
-      """
-      Check if the images are in sequential order
-      by comparing the ID of the next image
-      """
-      if image is not images[-1]:
-        next_image = images[images.index(image) + 1]
-        self.assertGreater(image.id_number, next_image.id_number)
+  for image in images:
+  # Check if the image IDs are listed in descending order
+  # by comparing the ID of the next image
+    if image is not images[-1]:
+      next_image = images[images.index(image) + 1]
+      assert image.id_number > next_image.id_number
 
-if __name__ == "__main__":
-  main()
