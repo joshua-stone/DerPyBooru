d7436c1825af4ed358fb5e98cea40cf63d0e5e34
joshua-stone <joshua.gage.stone@gmail.com>
2020-03-28 22:44:10 -0400

    Update __init__.py

--- derpibooru/__init__.py
+++ derpibooru/__init__.py
@@ -19,7 +19,7 @@ Library documentation is found at <https://github.com/joshua-stone/DerPyBooru>.
 """
 
 __title__ = "DerPyBooru"
-__version__ = "0.7.2"
+__version__ = "0.7.4"
 __author__ = "Joshua Stone"
 __license__ = "Simplified BSD Licence"
 __copyright__ = "Copyright (c) 2014, Joshua Stone"
